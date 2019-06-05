306537845f4e1526cc2f7042d7b6096ca86cf782
saini-g <saini.gaurav2205@gmail.com>
2019-06-05 22:19:29 +0530

    changed controller implementation

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Botkit = require('botkit');
 const { WatsonMiddleware } = require('botkit-middleware-watson');
+const { SlackAdapter } = require('botbuilder-adapter-slack');
 const mongoProvider = require('./db/mongo-provider')({
     mongoUri: process.env.MONGO_CONNECTION_STRING
 });
@@ -9,15 +10,24 @@ const basicListener = require('./listeners/basic-ears');
 const interactiveListener = require('./listeners/interactive');
 const { getFilterMiddleware } = require('./listeners/middleware/migration-filter');
 
-let botCfg = {
+const adapter = new SlackAdapter({
+    clientSigningSecret: process.env.SLACK_SIGNING_SECRET,
+    botToken: 'xoxb-483103024786-507228297601-8erOocRM12S2uOwIknTxfFYv'
+});
+
+/* let botCfg = {
     clientId: process.env.SLACK_CLIENT_ID,
     clientSecret: process.env.SLACK_CLIENT_SECRET,
     scopes: ['bot', 'team:read', 'users:read', 'users:read.email', 'channels:write'],
     storage: mongoProvider,
     clientSigningSecret: process.env.SLACK_SIGNING_SECRET
-};
+}; */
 
-let controller = Botkit.slackbot(botCfg);
+// let controller = Botkit.slackbot(botCfg);
+const controller = new Botkit({
+    adapter
+    // ...other options
+});
 controller.startTicking();
 controller.middleware.receive.use(getFilterMiddleware(controller));
 
@@ -27,9 +37,6 @@ const watsonMiddleware = new WatsonMiddleware({
     workspace_id: process.env.WATSON_WS_ID,
     version: '2018-07-10',
 });
-console.log(watsonMiddleware);
-console.log('...........');
-console.log(watsonMiddleware.receive);
 controller.middleware.receive.use(watsonMiddleware.receive.bind(watsonMiddleware));
 
 eventListeners(controller);
