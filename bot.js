511f064fa4eadbfc1208eceeabc657a522c1e82e
saini-g <saini.gaurav2205@gmail.com>
2019-07-22 16:26:12 +0530

    testing nlp using dialogflow

--- bot.js
+++ bot.js
@@ -2,10 +2,11 @@ const Botkit = require('botkit');
 const mongoProvider = require('./db/mongo-provider')({
     mongoUri: process.env.MONGO_CONNECTION_STRING
 });
+const dialogflowMiddleware = require('botkit-middleware-dialogflow')({});
 
-const eventListeners = require('./listeners/events');
-const basicListener = require('./listeners/basic-ears');
-const interactiveListener = require('./listeners/interactive');
+// const eventListeners = require('./listeners/events');
+// const basicListener = require('./listeners/basic-ears');
+// const interactiveListener = require('./listeners/interactive');
 const { getFilterMiddleware } = require('./listeners/middleware/migration-filter');
 
 let botCfg = {
@@ -19,9 +20,23 @@ let botCfg = {
 let controller = Botkit.slackbot(botCfg);
 controller.startTicking();
 controller.middleware.receive.use(getFilterMiddleware(controller));
+controller.middleware.receive.use(dialogflowMiddleware.receive);
 
-eventListeners(controller);
-basicListener(controller);
-interactiveListener(controller);
+// eventListeners(controller);
+// basicListener(controller);
+// interactiveListener(controller);
+
+controller.hears(
+    'create_nomination',
+    'direct_message,direct_mention',
+    dialogflowMiddleware.hears,
+    (bot, message) => {
+        bot.reply(message, message.fulfillment.text);
+        console.log(message.intent);
+        console.log(message.entities);
+        console.log(message.fulfillment);
+        console.log(message.confidence);
+    }
+);
 
 module.exports = controller;
