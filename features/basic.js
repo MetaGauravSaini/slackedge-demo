1cf896e8d93b396170a34f79233bd61d104c6bd8
saini-g <saini.gaurav2205@gmail.com>
2019-07-26 15:28:00 +0530

    testing pattern recognition

--- features/basic.js
+++ features/basic.js
@@ -1,37 +1,13 @@
-const dialogflowMiddleware = require('../df-middleware');
 
 module.exports = function(controller) {
-
-    controller.hears(
-        ['can you nominate an account?'],
-        ['message', 'direct_message'],
-        dialogflowMiddleware.hears,
-        async (bot, message) => {
-            console.log('df response----');
-            console.log(message.intent, message.entities, message.fulfillment);
-            await bot.reply(message,{ text: 'I HEARD ALL CAPS!' });
-        }
-    );
 
     controller.on(
         'direct_message',
         async (bot, message) => {
             console.log('df response----');
             console.log(message.intent, message.entities, message.fulfillment);
-            await bot.reply(message, 'I heard a private message');
+            await bot.reply(message, `intent detected - ${message.intent}`);
         }
     );
 
 }
-
-/* FULL OAUTH DETAILS { ok: true,
-  access_token:
-   'xoxp-483103024786-482498576433-707476790775-43fe85ec9506f26035cc1a180d0b6931',
-  scope:
-   'identify,bot,team:read,users:read,users:read.email,channels:write',
-  user_id: 'UE6ENGYCR',
-  team_name: 'POR',
-  team_id: 'TE7310QP4',
-  bot:
-   { bot_user_id: 'UEX6Q8RHP',
-     bot_access_token: 'xoxb-483103024786-507228297601-VSjDgMNOWsabBJaZkXrLdsRp' } } */
